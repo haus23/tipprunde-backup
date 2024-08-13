aed35f47c41cb81d684e7a1914cef4afc0227b0f
Micha Buchholz <micha@haus23.net>
2024-08-13 09:52:31 +0200

    Fix formatting and linting errors.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+const rootContainer = document.getElementById('root');
+
+if (!rootContainer) {
+  throw new Error('Missing #root element in body!');
+}
+
+createRoot(rootContainer).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
