8974b2fbd820d9aeb651be1c6f1fd99446ccd7a7
Micha Buchholz <micha@haus23.net>
2024-08-13 10:30:31 +0200

    refactor: Clean up and rebrand the app.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
 import App from './App.tsx';
-import './index.css';
+import './styles.css';
 
 const rootContainer = document.getElementById('root');
 
