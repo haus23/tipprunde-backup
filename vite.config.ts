969bd49d6d75b9f46c4e5d1e949d15aae114b135
Micha Buchholz <micha@haus23.net>
2024-08-13 15:20:05 +0200

    chore(dx): Configure path alias.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), react(), tsconfigPaths()],
 });
