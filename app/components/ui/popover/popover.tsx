9a51bf92092f0c07e1365b833ad1b1b37fb67097
Micha Buchholz <micha@haus23.net>
2024-08-15 12:21:43 +0200

    feat: Add popover arrow.

--- app/components/ui/popover/popover.tsx
+++ app/components/ui/popover/popover.tsx
@@ -1,4 +1,6 @@
+import type { ReactNode } from 'react';
 import {
+  OverlayArrow,
   type PopoverProps,
   Popover as RACPopover,
   composeRenderProps,
@@ -6,20 +8,46 @@ import {
 import { tv } from '#utils/tv';
 
 const popoverStyles = tv({
-  base: ['rounded-default bg-content-1 shadow-default'],
+  base: ['group rounded-default bg-popover shadow-default'],
 });
 
 namespace Popover {
-  export interface Props extends PopoverProps {}
+  export interface Props extends PopoverProps {
+    children: ReactNode;
+    withArrow?: boolean | undefined;
+  }
 }
 
-export function Popover({ className, ...props }: Popover.Props) {
+export function Popover({
+  className,
+  withArrow,
+  children,
+  ...props
+}: Popover.Props) {
   return (
     <RACPopover
       className={composeRenderProps(className, (className, renderProps) =>
         popoverStyles({ ...renderProps, className }),
       )}
+      containerPadding={8}
+      offset={withArrow ? 12 : 8}
       {...props}
-    />
+    >
+      {withArrow && (
+        <OverlayArrow className="group">
+          <svg
+            role="img"
+            aria-label="Pfeil, der auf den Trigger zeigt"
+            width={12}
+            height={12}
+            viewBox="0 0 12 12"
+            className="group-data-[placement=left]:-rotate-90 block fill-popover stroke-1 stroke-popover group-data-[placement=bottom]:rotate-180 group-data-[placement=right]:rotate-90"
+          >
+            <path d="M0 0 L6 6 L12 0" />
+          </svg>
+        </OverlayArrow>
+      )}
+      {children}
+    </RACPopover>
   );
 }
--- app/routes/kitchen-sink+/$feature+/popovers.tsx
+++ app/routes/kitchen-sink+/$feature+/popovers.tsx
@@ -5,6 +5,8 @@ import { Popover } from '#components/ui/popover/popover';
 export default function PopoversRoute() {
   const [defaultOpen, setDefaultOpen] = useState(false);
   const defaultToggleRef = useRef(null);
+  const [withArrowOpen, setWithArrowOpen] = useState(false);
+  const withArrowToggleRef = useRef(null);
 
   return (
     <div className="flex flex-col items-center gap-y-8 text-center">
@@ -22,6 +24,25 @@ export default function PopoversRoute() {
           triggerRef={defaultToggleRef}
           isOpen={defaultOpen}
           onOpenChange={setDefaultOpen}
+        >
+          <p className="p-4">This is my Popover Content</p>
+        </Popover>
+      </div>
+      <div className="space-y-2">
+        <h3 className="flex items-center gap-x-4">
+          <span>With Arrow:</span>
+          <Button
+            onPress={() => setWithArrowOpen((state) => !state)}
+            ref={withArrowToggleRef}
+          >
+            Toggle
+          </Button>
+        </h3>
+        <Popover
+          triggerRef={withArrowToggleRef}
+          isOpen={withArrowOpen}
+          onOpenChange={setWithArrowOpen}
+          withArrow
         >
           <p className="p-4">This is my Popover Content</p>
         </Popover>
