afa7c87e0e006306ab4ab6be8d7ccaeab71fc3ef
Micha Buchholz <micha@haus23.net>
2024-08-15 21:56:44 +0200

    feat: Add first navlink iteration.

--- app/components/ui/link/link.tsx
+++ app/components/ui/link/link.tsx
@@ -4,12 +4,23 @@ import {
   composeRenderProps,
 } from 'react-aria-components';
 
+import { useLocation, useResolvedPath } from 'react-router-dom';
 import { tv } from '#utils/tv';
 import { focusRingStyles } from '../common-styles';
 
 const linkStyles = tv({
   extend: focusRingStyles,
-  base: ['rounded-default transition-opacity hover:opacity-hover'],
+  base: [
+    'inline-flex items-center justify-center rounded-default px-4 py-1 font-medium text-app-accent transition-opacity hover:opacity-hover',
+  ],
+});
+
+const navLinkStyles = tv({
+  extend: linkStyles,
+  base: ['transition-colors'],
+  variants: {
+    isCurrent: { true: 'text-active' },
+  },
 });
 
 namespace Link {
@@ -25,6 +36,31 @@ export function Link({ className, href, ...props }: Link.Props) {
       href={href}
       className={composeRenderProps(className, (className, renderProps) =>
         linkStyles({ ...renderProps, className }),
+      )}
+    />
+  );
+}
+
+namespace NavLink {
+  export interface Props extends LinkProps {
+    href: string;
+  }
+}
+
+export function NavLink({ className, href, ...props }: NavLink.Props) {
+  // See: https://github.com/remix-run/react-router/blob/main/packages/react-router-dom/index.tsx#L1030
+  const path = useResolvedPath(href);
+  const location = useLocation();
+
+  const isActive = path.pathname === location.pathname;
+
+  return (
+    <RACLink
+      {...(isActive && { 'aria-current': 'page' })}
+      {...props}
+      href={href}
+      className={composeRenderProps(className, (className, renderProps) =>
+        navLinkStyles({ ...renderProps, className }),
       )}
     />
   );
--- app/routes/kitchen-sink+/$feature+/links.tsx
+++ app/routes/kitchen-sink+/$feature+/links.tsx
@@ -1,18 +1,26 @@
 import { useParams } from 'react-router-dom';
-import { Link } from '#components/ui/link/link';
+import { Link, NavLink } from '#components/ui/link/link';
 
 export default function LinksRoute() {
   const { feature } = useParams() as { feature: string };
 
   return (
-    <div className="flex flex-col items-center gap-y-8 text-center">
-      <div className="flex flex-col gap-y-2">
+    <div className="space-y-8 text-center">
+      <div className="space-y-2">
         <h3>Default Link</h3>
         <Link href={`/kitchen-sink/${feature}/icons`}>Icons</Link>
       </div>
-      <div className="flex flex-col gap-y-2">
+      <div className="space-y-2">
         <h3>Link to 404</h3>
         <Link href={`/kitchen-sink/${feature}/wont-fix`}>404</Link>
+      </div>
+      <div className="space-y-2">
+        <h3>NavLink to current page</h3>
+        <NavLink href={`/kitchen-sink/${feature}/links`}>Links</NavLink>
+      </div>
+      <div className="space-y-2">
+        <h3>NavLink to buttons page</h3>
+        <NavLink href={`/kitchen-sink/${feature}/buttons`}>Buttons</NavLink>
       </div>
     </div>
   );
